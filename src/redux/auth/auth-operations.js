e4c5ae40473f0de5d84e2b22d260befa562b14b0
Oleg <potap.oleh.v@gmail.com>
2022-07-27 15:30:30 +0300

    initial

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+import authAPI from "../../shared/api/authApi";
+
+export const signup = createAsyncThunk(
+    "auth/signup", async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+);
+
+export const login = createAsyncThunk(
+    "auth/login", async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+)
+
+export const logout = createAsyncThunk(
+    "auth/logout", async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+);
+
+export const current = createAsyncThunk(
+    "auth/current",
+    async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+)

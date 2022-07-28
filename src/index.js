ee26f40dd126527fbd88e240c3950c00bdc21dea
OlenaPustova <i.am.elena.p@gmail.com>
2022-07-28 11:59:39 +0300

    adds sprite, fonts, common styles

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import './index.scss';
 import App from './App';
-import "modern-normalize";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-   </Provider>
+  </Provider>
   // </React.StrictMode>
 );

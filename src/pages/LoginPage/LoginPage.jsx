66f94b19f2513beee81c9376b3a1a44f0ef34e22
s3bull <stribla@yahoo.com>
2022-07-28 19:52:14 +0300

    features/loginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,14 @@
+import { LoginBG } from "./LoginBG/LoginBG";
+import { LoginForm } from "./LoginForm/LoginForm";
+import s from "./LoginPage.module.scss";
+
 const LoginPage = () => {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
-    }
-    
-    export default LoginPage
+  return (
+    <div className={s.container}>
+      <LoginForm className={s.form} />
+      <LoginBG />
+    </div>
+  );
+};
+
+export default LoginPage;

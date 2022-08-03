c49f8d52c544d8fd6b280f2b83c92f81618b88cf
Oleg <potap.oleh.v@gmail.com>
2022-08-03 16:43:09 +0300

    CalculatorCalories-component

--- src/components/CalculatorСalorieForm/CalculatorCalorieForm.jsx
+++ src/components/CalculatorСalorieForm/CalculatorCalorieForm.jsx
@@ -1,12 +1,21 @@
 import s from './CalculatorCalorieForm.module.scss';
 import Loader from '../../components/common/Loader/Loader';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useSelector } from 'react-redux';
 import { getIsLoading } from '../../redux/auth/auth-selectors';
+import * as Yup from 'yup';
 
 const CalculatorCalorieForm = () => {
   // const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
+  const schema = Yup.object().shape({
+    height: Yup.number().required(),
+    desiredWeight: Yup.number().required(),
+    age: Yup.number().required(),
+    currentWeight: Yup.number().required(),
+    bloodType: Yup.string().required(),
+  });
+
   return (
     <div className={s.section}>
       {isLoading && <Loader />}
@@ -21,6 +30,7 @@ const CalculatorCalorieForm = () => {
           currentWeight: '',
           bloodType: '',
         }}
+        validationSchema={schema}
         onSubmit={values => {
           // Тут я так понимаю если пользователь залогинен
           // нужно оправить эти данные на бек
@@ -32,7 +42,9 @@ const CalculatorCalorieForm = () => {
         <Form className={s.form}>
           <div className={s.fieldsWrapper}>
             <div className={s.fieldWrapper}>
-              <label htmlFor="height">Height *</label>
+              <label className={s.textLabel} htmlFor="height">
+                Height *
+              </label>
               <Field
                 className={s.input}
                 id="height"
@@ -40,9 +52,15 @@ const CalculatorCalorieForm = () => {
                 type="number"
                 autoComplete="off"
               />
+              <ErrorMessage
+                name="height"
+                render={() => <p>Field is required</p>}
+              />
             </div>
             <div className={s.fieldWrapper}>
-              <label htmlFor="desiredWeight">Desired Weight *</label>
+              <label className={s.textLabel} htmlFor="desiredWeight">
+                Desired Weight *
+              </label>
               <Field
                 className={s.input}
                 id="desiredWeight"
@@ -50,9 +68,15 @@ const CalculatorCalorieForm = () => {
                 type="number"
                 autoComplete="off"
               />
+              <ErrorMessage
+                name="desiredWeight"
+                render={() => <p>Field is required</p>}
+              />
             </div>
             <div className={s.fieldWrapper}>
-              <label htmlFor="age">Age *</label>
+              <label className={s.textLabel} htmlFor="age">
+                Age *
+              </label>
               <Field
                 className={s.input}
                 id="age"
@@ -60,36 +84,53 @@ const CalculatorCalorieForm = () => {
                 type="number"
                 autoComplete="off"
               />
+              <ErrorMessage
+                name="age"
+                render={() => <p>Field is required</p>}
+              />
             </div>
             <div className={s.fieldWrapper}>
-              <div id="radio-group">Blood Type *</div>
-              <div role="group" aria-labelledby="radio-group">
-                <label>
-                  <Field type="radio" name="bloodType" value="1" />1
+              <div className={s.textLabel} id="radio-group">
+                Blood Type *
+              </div>
+              <div className={s.radioGroup} role="group" aria-labelledby="radio-group">
+                <label className={s.radioLabel}>
+                  <Field className={s.radoiButton} type="radio" name="bloodType" value="1" /> <span> 1</span>
                 </label>
-                <label>
-                  <Field type="radio" name="bloodType" value="2" />2
+                <label className={s.radioLabel}>
+                  <Field className={s.radoiButton} type="radio" name="bloodType" value="2" /> <span> 2</span>
                 </label>
-                <label>
-                  <Field type="radio" name="bloodType" value="3" />3
+                <label className={s.radioLabel}>
+                  <Field className={s.radoiButton} type="radio" name="bloodType" value="3" /> <span> 3</span>
                 </label>
-                <label>
-                  <Field type="radio" name="bloodType" value="4" />4
+                <label className={s.radioLabel}>
+                  <Field className={s.radoiButton} type="radio" name="bloodType" value="4" /> <span> 4</span>
                 </label>
               </div>
-              <div className={s.fieldWrapper}>
-                <label htmlFor="currentWeight">Current Weight *</label>
-                <Field
-                  className={s.input}
-                  id="currentWeight"
-                  name="currentWeight"
-                  type="number"
-                  autoComplete="off"
-                />
-              </div>
+              <ErrorMessage
+                name="bloodType"
+                render={() => <p>Field is required</p>}
+              />
             </div>
-            <button type="submit">Submit</button>
+            <div className={s.fieldWrapper}>
+              <label className={s.textLabel} htmlFor="currentWeight">
+                Current Weight *
+              </label>
+              <Field
+                className={s.input}
+                id="currentWeight"
+                name="currentWeight"
+                type="number"
+                autoComplete="off"
+              />
+              <ErrorMessage
+                name="currentWeight"
+                render={() => <p>Field is required</p>}
+              />
+            </div>
+            <button className={s.submitButton} type="submit">Start losing Weight</button>
           </div>
+          
         </Form>
       </Formik>
     </div>

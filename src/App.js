ab156fd1d4c2ad3e5d8193e56d71ef247903a347
Oleg <potap.oleh.v@gmail.com>
2022-08-05 18:24:29 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import {
   // Link
 } from 'react-router-dom';
 import { lazy } from 'react';
-<<<<<<< HEAD
-=======
 import PublicRoute from './utils/PrivateRoute.js';
 import PrivateRoute from './utils/PublicRoute.js';
 import Header from './components/Header/Header';
->>>>>>> c72205d8c22d038fd7c134d15a88b688e82f70f9
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const RegisterPage = lazy(() =>
@@ -35,10 +32,10 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Route>
-        <Route element={<PrivateRoute />}>
+        {/* <Route element={<PrivateRoute />}> */}
           <Route path="/diary" element={<DiaryPage />} />
           <Route path="/calculator" element={<CalculatorPage />} />
-        </Route>
+        {/* </Route> */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import DiaryAddProductForm from '../../components/diaryAddProductForm/DiaryAddProductForm';
-import DiaryAddProductFormModal from '../../components/diaryAddProductForm/DiaryAddProductFormModal';
-import DiaryProductList from '../../components/diaryProductList/DiaryProductList';
+import Container from '../../components/common/Container';
+import PrivatePagesBG from '../../components/PrivatePagesBG';
+// import DiaryAddProductForm from '../../components/diaryProductList';
+// import DiaryAddProductFormModal from '../../components/diaryAddProductForm/DiaryAddProductFormModal';
+import DiaryProductList from '../../components/DiaryProductsList/DiaryProductList';
 import s from './DiaryPage.module.scss';
 // import { Component } from 'react';
-import Calendar from '../../components/diaryDateCalendar/DiaryDateCalendar';
+// import Calendar from '../../components/DiaryDateCalendar/DiaryDateCalendar';
 // import Autocomplete from "react-autocomplete";
 // import RightSideBar from '../../components/rightSideBar/RightSideBar';
 import useMedia from 'use-media';
@@ -23,30 +25,33 @@ const DiaryPage = () => {
   };
 
   return (
-    <section className={s.diarySection}>
-      {state && (
-        <DiaryAddProductFormModal closeModal={closeModal}>
-          <DiaryAddProductForm closeModal={closeModal} />
-        </DiaryAddProductFormModal>
-      )}
+    <Container>
+    <PrivatePagesBG />
+    </Container>
+    // <section className={s.diarySection}>
+    //   {state && (
+    //     <DiaryAddProductFormModal closeModal={closeModal}>
+    //       <DiaryAddProductForm closeModal={closeModal} />
+    //     </DiaryAddProductFormModal>
+    //   )}
 
-      <div className={s.container}>
-        <div className={s.dateAndCalendar}>
-          <Calendar />
-          <img className={s.calendarImage} src="./calendar 1.svg" alt="" />
-          <svg width="18" height="20" className={s.calendarImage}>
-            {/* подключить иконку календаря */}
-            {/* <use href="./symbol-defs.svg.svg#calendar"></use> */}
-          </svg>
-        </div>
+      // <div className={s.container}>
+      //   <div className={s.dateAndCalendar}>
+      //     {/* <Calendar /> */}
+      //     <img className={s.calendarImage} src="./calendar 1.svg" alt="" />
+      //     <svg width="18" height="20" className={s.calendarImage}>
+      //       {/* подключить иконку календаря */}
+      //       {/* <use href="./symbol-defs.svg.svg#calendar"></use> */}
+      //     </svg>
+      //   </div>
 
-        {isWide && <DiaryAddProductForm />}
+        // {/* {isWide && <DiaryAddProductForm />} */}
 
-        <DiaryProductList openModalProp={openModal} />
-      </div>
+        // <DiaryProductList openModalProp={openModal} />
+      // </div>
 
-      {/* <div className={s.summary}><RightSideBar /></div> */}
-    </section>
+      // {/* <div className={s.summary}><RightSideBar /></div> */}
+    // </section>
   );
 };
 
